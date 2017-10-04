aa94287d77aa604d1e340830509d40856fc46d53
Alessandro Arnodo <alessandro@arnodo.net>
2017-10-04 10:20:53 +0200

    do not use anonim function for bedder stack traces

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports = function (a, b) {
+module.exports = function isShallowEqual (a, b) {
   if (a === b) return true
   for (var i in a) if (!(i in b)) return false
   for (var i in b) if (a[i] !== b[i]) return false
--- test.js
+++ test.js
@@ -1,12 +1,12 @@
-var assert = require("assert")
-var isShallowEqual = require("./")
+var assert = require('assert')
+var isShallowEqual = require('./')
 
 // pass
 assert(
   isShallowEqual({ a: 1, b: 2, c: undefined }, { a: 1, b: 2, c: undefined })
 )
 
-assert(isShallowEqual({ a: 1, b: 2, c: 3 }, { a: 1, b: 2, c: 3 }));
+assert(isShallowEqual({ a: 1, b: 2, c: 3 }, { a: 1, b: 2, c: 3 }))
 
 var o = {}
 assert(isShallowEqual({ a: 1, b: 2, c: o }, { a: 1, b: 2, c: o }))
@@ -23,14 +23,14 @@ assert(
       a: 1,
       b: 2,
       d: function() {
-        return 1;
+        return 1
       }
     },
     {
       a: 1,
       b: 2,
       d: function() {
-        return 1;
+        return 1
       }
     }
   )
